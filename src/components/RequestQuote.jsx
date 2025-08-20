5e5314ec5090286413f0a61f7d01fb01e9426168
Natsumi26 <marion.redon26@yahoo.fr>
2025-08-20 14:13:17 +0200

    formulaire demande

--- src/components/RequestQuote.jsx
+++ src/components/RequestQuote.jsx
@@ -1,15 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { Link } from "react-router";
+import Nav from "./Nav";
 
 export default function RequestQuote() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [nom, setNom] = useState("");
   const [prénom, setPrénom] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [userId, setUserId] = useState(null);
+
+  const adaptCategories = (data) => {
+    console.log("Data reçue dans adaptCategories :", data);
+    return data.map((cat) => ({
+      id: cat.id,
+      nom: cat.nom
+    }))
+  }
 
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // const fetchUser = async () => {
+    //   try {
+    //     const res = await fetch("http://localhost:8000/api/me", {
+    //       headers: {
+    //         "Authorization": "Bearer " + localStorage.getItem("token"),
+    //       },
+    //     });
+    //     if (!res.ok) throw new Error("Impossible de récupérer l'utilisateur");
+    //     const data = await res.json();
+    //     setUserId(data.id); // ou data.user_id selon ton API
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // };
+    const fetchCategories = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/api/categories");
+        if (!res.ok) throw new Error("Erreur fetch catégories");
+        const data = await res.json();
+        
+        console.log(data.member);
+        setCategories(adaptCategories(data.member));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // fetchUser();
+    fetchCategories();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,11 +62,11 @@ export default function RequestQuote() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          email,
-          password,
           nom,
           prénom,
-          roles: ["ROLE_USER"],
+          description,
+          category,
+          // user_id: userId,
         }),
       });
 
@@ -40,6 +83,7 @@ export default function RequestQuote() {
   };
     return (
     <>
+      <Nav></Nav>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <Link to="/">
@@ -50,24 +94,23 @@ export default function RequestQuote() {
           />
           </Link>
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Créez votre compte
+            Faire une demande
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form method="POST" onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                Adresse e-mail
+              <label htmlFor="nom" className="block text-sm/6 font-medium text-gray-900">
+                Nom
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
+                  id="nom"
+                  name="nom"
+                  type="text"
                   required
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6"  value={email} onChange={(e)=> setEmail(e.target.value)}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6"  value={nom} onChange={(e)=> setNom(e.target.value)}
                 />
               </div>
             </div>
@@ -79,63 +122,42 @@ export default function RequestQuote() {
                 <input
                   id="prenom"
                   name="prénom"
-                  type="prénom"
+                  type="text"
                   required
                   className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300  focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6"  value={prénom} onChange={(e)=> setPrénom(e.target.value)}
                 />
               </div>
             </div>
                         <div>
-              <label htmlFor="nom" className="block text-sm/6 font-medium text-gray-900">
-                Nom
+              <label htmlFor="description" className="block text-sm/6 font-medium text-gray-900">
+                Description
               </label>
               <div className="mt-2">
-                <input
-                  id="nom"
-                  name="nom"
-                  type="nom"
+                <textarea
+                  id="description"
+                  name="description"
+                  type="textarea"
                   required
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300  focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6"  value={nom} onChange={(e)=> setNom(e.target.value)}
-                />
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300  focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6"  value={description} onChange={(e)=> setDescription(e.target.value)}
+                ></textarea>
               </div>
             </div>
             <div>
               <div>
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                  Mots de passe
+                <label htmlFor="category" className="block text-sm/6 font-medium text-gray-900">
+                  Catégorie
                 </label>
               </div>
               <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6" value={password} onChange={(e)=> setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-                <div>
-              {/* <label htmlFor="pseudo" className="block text-sm/6 font-medium text-gray-900">
-                Vérifiez votre mots de passe
-              </label> */}
-              {/* <div className="mt-2">
-                <input
-                  id="password2"
-                  name="password2"
-                  type="password"
-                  required
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300  focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200 sm:text-sm/6"
-                />
-              </div> */}
-              <div>
-                <input
-                  type="hidden"
-                  id="role"
-                  name="roles"
-                  value='["ROLE_USER"]'
-                />
+                <select name="category" id="category" value={category} onChange={(e)=> setCategory(e.target.value)}>
+                  <option value="">Toutes les catégories</option>
+                  {categories.map((cat) => (
+                    <option key={cat.id} value={cat.id}>
+                      {cat.nom}
+                    </option>
+                  ))}
+                </select>
+                
               </div>
             </div>
             <div>
@@ -143,7 +165,7 @@ export default function RequestQuote() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-rose-200 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-rose-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-300"
               >
-                S'enregistrer
+                Envoyer la demande
               </button>
             </div>
           </form>
