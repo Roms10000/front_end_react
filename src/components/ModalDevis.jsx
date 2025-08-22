e3d07ee2c219f37ff2b3ba5e370a2fbf7e110905
Roms10000 <romain.richardon@outlook.fr>
2025-08-22 11:17:19 +0200

    modif modaldevis

--- src/components/ModalDevis.jsx
+++ src/components/ModalDevis.jsx
@@ -135,7 +135,7 @@ const ModalContent = () => (
           </div>
           <div className="overflow-x-auto shadow-md rounded-lg mb-6">
             <table className="w-full text-sm text-left text-gray-500 ">
-              <thead className="text-xs text-gray-700 uppercase bg-rose-200 hover:bg-rose-300">
+              <thead className="text-xs text-gray-900 uppercase bg-rose-200 ">
                 <tr>
                   <th scope="col" className="px-6 py-3">Prestation</th>
                   <th scope="col" className="px-6 py-3">Quantité</th>
@@ -145,8 +145,8 @@ const ModalContent = () => (
               </thead>
               <tbody>
                 {data.devis.prestations.map((prestationItem, index) => (
-                  <tr key={index} className="bg-white border-b hover:bg-gray-50">
-                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{prestationItem.prestation.nom}</td>
+                  <tr key={index}>
+                    <td className="px-6 py-4">{prestationItem.prestation.nom}</td>
                     <td className="px-6 py-4">{prestationItem.quantity}</td>
                     <td className="px-6 py-4">{prestationItem.prestation.pu ? parseFloat(prestationItem.prestation.pu).toFixed(2) : '0.00'} €</td>
                     <td className="px-6 py-4">{prestationItem.soustotal ? parseFloat(prestationItem.soustotal).toFixed(2) : '0.00'} €</td>
@@ -155,16 +155,18 @@ const ModalContent = () => (
               </tbody>
             </table>
           </div>
-          <div className="flex justify-end p-4 border-t border-gray-200 dark:border-gray-600">
             <div className="text-right">
               <span className="text-lg font-bold text-gray-900 dark:text-white">Total : {data.devis?.total ? parseFloat(data.devis.total).toFixed(2) : '0.00'} €</span>
+          </div>
+          <div className="grid grid-cols-2 mt-5">
+            <div className='flex'>
+              <button onClick={handleStatutAccept} type="submit" className=' text-white bg-red-400 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center shadow-md hover:shadow-lg cursor-pointer'>Refuser</button>
+            </div>
+            <div className='flex justify-end'>
+              <button onClick={handleStatutCancel} type="submit" className=' text-white bg-green-300 hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center shadow-md hover:shadow-lg cursor-pointer'>Accepter</button>
             </div>
           </div>
-          <div className="mt-6 text-center text-gray-500 dark:text-gray-400">
-            <button onClick={handleStatutAccept} type="submit" className='inline-flex items-center justify-center text-white bg-red-200 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors duration-200 shadow-md hover:shadow-lg'>Refuser</button>
-            <p>Merci pour votre confiance. Veuillez nous contacter pour toute question.</p>
-            <button onClick={handleStatutCancel} type="submit" className='inline-flex items-center justify-center text-white bg-green-200 hover:bg-green-300 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-colors duration-200 shadow-md hover:shadow-lg'>Accepter</button>
-          </div>
+            <p className="mt-6 text-center text-gray-500 dark:text-gray-400">Merci pour votre confiance. Veuillez nous contacter pour toute question.</p>
         </div>
       </div>
     </div>
