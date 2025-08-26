2127cf1ed027eb337fb319fae964fa5bc62e1425
Roms10000 <romain.richardon@outlook.fr>
2025-08-26 15:37:39 +0200

    a

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router";
+import ForgotPassword from "./ForgotPassword";
 
 
 export default function Login () {
@@ -93,9 +94,9 @@ export default function Login () {
                   Mots de passe
                 </label>
                 <div className="text-sm">
-                  <a href="http://localhost:5173/forgotPassword" className="font-semibold text-rose-200 hover:text-rose-300">
+                  <Link to="/forgotPassword" className="font-semibold text-rose-200 hover:text-rose-300">
                     Mots de passe oublié ?
-                  </a>
+                  </Link>
                 </div>
               </div>
               <div className="mt-2">
@@ -122,7 +123,7 @@ export default function Login () {
 
           <p className="mt-10 text-center text-sm/6 text-gray-500">
             Pas encore membre ?{' '}
-            <Link to="register" className="font-semibold text-rose-200 hover:text-amber-500">
+            <Link to="/register" className="font-semibold text-rose-200 hover:text-amber-500">
               S'inscrire
             </Link>
           </p>
