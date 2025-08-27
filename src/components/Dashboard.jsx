c7f609353d36f9bea5a2d94cf058c11ce99f1eab
Natsumi26 <marion.redon26@yahoo.fr>
2025-08-27 10:08:38 +0200

    paiement paypal

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,9 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
 import ModalDevis from "./ModalDevis";
-import { button } from "@material-tailwind/react";
+import { Button, button } from "@material-tailwind/react";
 import React, { useState, useEffect } from "react";
+import ModalPayPal from "./ModalPayPal";
 
 export default function Dashboard ({Id}) {
 
@@ -12,32 +13,29 @@ export default function Dashboard ({Id}) {
     const [userRoles, setUserRoles] = useState([]);
 
 
-  useEffect(() => {
+useEffect(() => {
 
 const userId = localStorage.getItem("id");
 const roleString = localStorage.getItem("roles")
 let roles = [];
     try {
-      if (roleString && roleString !== "undefined") {
+        if (roleString && roleString !== "undefined") {
         roles = JSON.parse(roleString);
-      }
+        }
     } catch (e) {
-      console.error("Erreur lors de l'analyse des rôles depuis le localStorage :", e);
+        console.error("Erreur lors de l'analyse des rôles depuis le localStorage :", e);
     }
     setUserRoles(roles);
 
     if (!userId || roles.length === 0) {
-      console.log("Utilisateur non trouvable");
-      return;
+        console.log("Utilisateur non trouvable");
+        return;
     }
     
- const fetchDemandes= async () => {
+const fetchDemandes= async () => {
     try{
-        const isAdmin = roles.includes("ROLE_ADMIN");
         
-        const url = isAdmin
-          ? "http://localhost:8000/api/demandes"
-          : `http://localhost:8000/api/backoffice/${userId}`;
+        const url = `http://localhost:8000/api/backoffice/${userId}`;
         
             const res = await fetch(url);
             if (!res.ok) throw new Error("Erreur fetch demandes");
@@ -47,23 +45,23 @@ let roles = [];
         } catch (error) {
             console.error(error);
         }
-  };
-      fetchDemandes();
-  }, []);
-
+    };
+        fetchDemandes();
+    }, []);
+    
 
 
 const isAdmin = userRoles.includes("ROLE_ADMIN");
 return(
 <div className="flex flex-col min-h-[130vh]">
 <Nav />
- <main className="flex-grow">
-    <div>
-        <div className="flex ml-30 mt-10 underline">
-            <h5 className=" flex mb-10  text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white great-vibes-regular">Suivie des demandes</h5>
-            <svg xmlns="http://www.w3.org/2000/svg" width="128" height="45" viewBox="0 0 128 128">
-            <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize="96">🌸</text></svg>
-        </div>
+    <main className="flex-grow">
+        <div>
+            <div className="flex ml-30 mt-10 underline">
+                <h5 className=" flex mb-10  text-2xl font-bold tracking-tight text-gray-900 group-hover:text-white great-vibes-regular">Suivie des demandes</h5>
+                <svg xmlns="http://www.w3.org/2000/svg" width="128" height="45" viewBox="0 0 128 128">
+                <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize="96">🌸</text></svg>
+            </div>
             <table className=" ml-18  justify-center w-400 border rounded-lg border-gray-900 text-sm text-gray-900">
                 <thead className="bg-gray-200 text-gray-900">
                     <tr>
@@ -79,6 +77,7 @@ return(
                         <th className="px-4 py-2 text-left">Statut du devis</th>
                         <th className="px-4 py-2 text-left">Devis</th>
                         <th className="px-4 py-2 text-left">Facture</th>
+                        <th className="px-4 py-2 text-left">Payement</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -104,24 +103,25 @@ return(
                         <td className="px-4 py-2">
                         {demande.facture!=null ? (
                                 <a
-                                  key={demande.facture}
-                                  href={`http://localhost:8000${demande.facture}`} // chemin vers le PDF
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                  className="text-rose-200 underline"
+                                key={demande.facture}
+                                href={`http://localhost:8000${demande.facture}`} // chemin vers le PDF
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-rose-200 underline"
                                 >
-                                  Voir la facture
+                                Voir la facture
                                 </a>
                         ) : (
-                          "--"
+                        "--"
                         )}
                         </td>
+                        <td className="px-4 py-2"><ModalPayPal factureId={demande.factureId} prixtotal={demande.total}/></td>
                     </tr>
                 ))}
                 </tbody>
             </table>
-    </div>
-</main>
+        </div>
+    </main>
 <Footer/>
 </div>
     );
